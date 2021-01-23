97f677b2e658d3fc9105d1547183c435380cce35
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2021-01-23 22:13:33 +0100

    Mod initialization

---
+++ buildBMBF.ps1
@@ -0,0 +1,15 @@
+# Builds a .zip file for loading with BMBF
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $buildScript += ".cmd"
+}
+
+& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
+Compress-Archive -Path  "./libs/arm64-v8a/libmenupillowquest.so",`
+                        "./bmbfmod.json",`
+                        "./extern/libbeatsaber-hook_1_0_10.so",`
+                        "./extern/libbs-utils.so",`
+                        "./extern/libquestui.so",`
+                        "./extern/libcustom-types.so", -DestinationPath "./MenuPillowQuest.zip" -Update
